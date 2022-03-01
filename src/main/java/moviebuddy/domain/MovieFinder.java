3fce9f0f3bf54de4356d00bf495c94ec267155cf
momo02 <jungha0902@gmail.com>
2022-03-01 21:47:41 +0900

    section_7: Use annotation-based configuration to dependency injection - 자동 의존 관계 주입 (@Autowired) : 빈 이름으로 의존 관계 주입 (@Qualifier 로 지정한 빈을 주입)

--- src/main/java/moviebuddy/domain/MovieFinder.java
+++ src/main/java/moviebuddy/domain/MovieFinder.java
@@ -1,6 +1,6 @@
 package moviebuddy.domain;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,7 +13,7 @@ public class MovieFinder {
 
     // @Autowired 어노테이션을 이용해 의존 관계 주입을 자동으로 받을 수 있도록 설정.
     // @Autowired  // 생성자가 하나뿐이라면 생략 가능.
-    public MovieFinder(MovieReader movieReader){
+    public MovieFinder(@Qualifier("csvMovieReader") MovieReader movieReader){
         this.movieReader = Objects.requireNonNull(movieReader);
     }
     // 기본적으론 선언되어 있는 타입(MovieReader)을 기반으로 의존 관계 주입 대상을 찾는다.
