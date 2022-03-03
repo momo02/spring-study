e5b3f6a6798bc768e17e9c20ac60369495a198e9
momo02 <jungha0902@gmail.com>
2022-03-03 20:21:22 +0900

    section_13: Make obtain metadata location from environment / 03. @PropertySource 애노테이션을 사용한 애플리케이션 프로퍼티 관리하기

--- src/main/java/moviebuddy/MovieBuddyFactory.java
+++ src/main/java/moviebuddy/MovieBuddyFactory.java
@@ -1,9 +1,6 @@
 package moviebuddy;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.*;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 
 /**
@@ -11,6 +8,7 @@ import org.springframework.oxm.jaxb.Jaxb2Marshaller;
  * 스프링의 빈 구성정보 => Configuration Metadata로 사용이 될 수 있음을 선언.
  */
 @Configuration
+@PropertySource("/application.properties")
 // 스프링은 자동 클래스 탐지 기법으로 스프링 컨테이너에 빈을 자동으로 등록해 주는 기능을 제공.
 // @ComponentScan 을 통해 활성화. -> 지정된 패키지 경로에서 @Component 와 같이 스테레오 타입으로 선언된 클래스를 찾아 빈으로 동록하고 관리.
 // 패키지를 지정하지 않으면 @ComponentScan 이 선언된 클래스의 패키지 경로를 기준으로 탐색.
--- src/test/java/moviebuddy/data/XmlMovieReaderTest.java
+++ src/test/java/moviebuddy/data/XmlMovieReaderTest.java
@@ -7,12 +7,15 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 import java.util.List;
 
 @ActiveProfiles(MovieBuddyProfile.XML_MODE)
 @SpringJUnitConfig(MovieBuddyFactory.class)
+// 테스트를 위한 프로퍼티 소스를 제공. (파일 생성 없이, 아래와 같이 간단하게 설정 가능)
+@TestPropertySource(properties = "movie.metadata=movie_metadata.xml")
 public class XmlMovieReaderTest {
 
     @Autowired
