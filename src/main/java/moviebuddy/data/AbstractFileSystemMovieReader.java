1252bf1f0a2c9df95b4bdda374bf1648e389283f
momo02 <jungha0902@gmail.com>
2022-03-03 20:45:22 +0900

    section_14: Refactor using resource interface for metadata handling / 01. 메타데이터 위치 설정의 문제점

--- src/main/java/moviebuddy/data/AbstractFileSystemMovieReader.java
+++ src/main/java/moviebuddy/data/AbstractFileSystemMovieReader.java
@@ -31,9 +31,27 @@ public abstract class AbstractFileSystemMovieReader implements MovieReader {
 		this.metadata = Objects.requireNonNull(metadata, "metadata is a required value.");
 	}
 
+
+	public URL getMetadataUrl() {
+		String location = getMetadata();
+		if(location.startsWith("file:")) {
+			// file URL 처리
+		}else if(location.startsWith("http:")) {
+			// http URL 처리
+		}
+		return ClassLoader.getSystemResource(location);
+	}
+
 	@PostConstruct
 	public void afterPropertiesSet() throws Exception {
-		URL metadataUrl = ClassLoader.getSystemResource(getMetadata());
+		// ClassLoader.getSystemResource(getMetadata()) -> 클래스패스 상의 자원만 처리할 수 있다.
+		// 만약 애플리케이션 외부 파일이나 네트워크 상의 파일을 읽어야 한다면? (ex. file:~, http:~, ftp:~)
+		// 프로토콜에 따라서 URL 객체를 다루기 위한 방식이 변경이 되어야 할 것.
+		// 이것도 한계가 존재함. 자바의 URL 클래스로는 자바 웹 애플리케이션 개발 시 사용되는 서블릿 컨텍스트 경로나 클라우드 스토리지 서비스에 있는 자원과 같은 것들은 표현할 수 없기 때문.
+		// =>>> 그래서 스프링은 일관된 방식으로 저 수준의 자원에 접근할 수 있는 서비스 추상화를 제공.
+
+		// URL metadataUrl = ClassLoader.getSystemResource(getMetadata());
+		URL metadataUrl = getMetadataUrl();
 		if (Objects.isNull(metadataUrl)) {
 			throw new FileNotFoundException(metadata);
 		}
